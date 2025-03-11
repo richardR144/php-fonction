e57fd7bcafb648573823a599b22a1fd3b08a2157
richardR144 <richard.rigollet@lapiscine.pro>
2025-03-11 11:19:52 +0100

    fonction exo 1 suite

--- index.php
+++ index.php
@@ -13,9 +13,26 @@ $article2 = [
     'content' => 'This is the content of article 2'
 ];
 
+
 $articles = [$article1, $article2];
-echo $articles[0]['title'];
-echo $articles[0]['content'];   
+foreach ($articles as $article) {
+    echo "<h2>" . $article['title'] . "</h2>";
+    echo "<p>" . $article['content'] . "</p>";
+    echo "<hr>"; 
+}
+
+//ou foreach ($articles as $article) {
+ //   if (!empty($article['title']) && !empty($article['content'])) { si on veut afficher que les articles qui ont un titre et un contenu
+ //       echo "<h2>" . $article['title'] . "</h2>";                  et qu'il n'y a pas de titre ou de contenu vide
+ //       echo "<p>" . $article['content'] . "</p>";
+  //      echo "<hr>"; 
+  //  }
+//}
+ //for ($i = 0; $i < count($articles); $i++) {
+  //  echo "<h2>" . $articles[$i]['title'] . "</h2>";
+   // echo "<p>" . $articles[$i]['content'] . "</p>";
+    //echo "<hr>";
+//}
 
 
 
